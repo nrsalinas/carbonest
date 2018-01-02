c28b8be0be7d02c48aaa2b760c5cd70f091a2f9f
Nelson Salinas <nrsalinas@gmail.com>
2018-01-02 16:14:00 -0500

    Typo.

--- Depuracion_datos_mixtos.py
+++ Depuracion_datos_mixtos.py
@@ -2,20 +2,20 @@
 # coding: utf-8
 
 # # Depuracion e integracion de datos mixtos
-# 
+#
 # El presente notebook realiza la exploración inicial y depuración de datos agregados de vegetación consolidados por el SMBYC.
-# 
+#
 # **Módulos requeridos:** Numpy y Pandas para la depuración de datos, SQLAlchemy para la inserción de información en las bases de datos.
-# 
+#
 # Las siguientes variables indican la ubicación de los archivos que contienen los datos sin depurar:
-# 
+#
 # 1. `asignacion`: Tabla en formato csv. Contiene información básica de las parcelas (ubicación, custodio, etc.).
-# 
-# 2. `individuos`: Tabla en formato csv. Contiene medidas y demás datos relacionados con los individuos (diámetro, altura, parcela, placa, etc.). 
-# 
-# También es necesario indicar las credenciales de accesos al servidor MySQL a través de las variables `user` (nombre de usuario) y `password` (clave de acceso).  
-# 
-# 
+#
+# 2. `individuos`: Tabla en formato csv. Contiene medidas y demás datos relacionados con los individuos (diámetro, altura, parcela, placa, etc.).
+#
+# También es necesario indicar las credenciales de accesos al servidor MySQL a través de las variables `user` (nombre de usuario) y `password` (clave de acceso).
+#
+#
 
 # In[ ]:
 
@@ -24,8 +24,8 @@ import pandas as pd
 import numpy as np
 
 # MySQL user and password
-password = u"Soledad1"
-user = u"root"
+password = u""
+user = u""
 
 # Asignar nombres de archivos a variables
 asignacion = "../data/quimera/asignacion.csv"
@@ -38,11 +38,11 @@ individuos =  "../data/quimera/newind.csv"
 asig = pd.read_csv(asignacion, encoding = 'utf8')
 
 # low_memory asume que los datos estan homogeneamente tipificados.
-ind = pd.read_csv(individuos, low_memory=False, encoding = 'utf8') 
+ind = pd.read_csv(individuos, low_memory=False, encoding = 'utf8')
 
 
 # ### Variables para los nombres de las columnas
-# 
+#
 # A continuación se adjudica una variable a cada nombre de la columna en ambos archivos. Si los nombres no son los mencionados deben ser actualizados en concordancia. El nombre de la columna que contiene el indice de la parcela debe ser igual en ambos archivos.
 
 # In[ ]:
@@ -120,11 +120,11 @@ fields_asig = [Plot, Area, Year, Tipo_parcela, Custodio, Custodio_abreviado, Par
 for fi in [FID, Plot]:
     if ind[fi].dtype != np.int64:
         print "Campo {0} tiene tipo inapropiado ({1} en vez de int64).".format(fi, ind[fi].dtype)
-        
+
 for fi in [D, H, X, Y, Densidad]:
     if ind[fi].dtype != np.float64:
         print "Campo {0} tiene tipo inapropiado ({1}en vez de float64).".format(fi, ind[fi].dtype)
-        
+
 for fi in [Placa, Fuente_densidad, Habito, Familia, Autor_familia, Genero, Autor_genero, Estado_epiteto, Epiteto, Autor_especie, Morfoespecie]:
     non_strings = ind[fi].dropna()[~ind[fi].dropna().apply(type).eq(unicode)]
     if len(non_strings):
@@ -144,9 +144,9 @@ if ind[Entra_calculos].dtype != np.bool_:
     print "Campo {0} tiene tipo inapropiado ({1} en vez de bool).".format(Entra_calculos, ind[Entra_calculos].dtype)
 
 
-# Verificacion de tipo de datos en tabla asignacion    
+# Verificacion de tipo de datos en tabla asignacion
 
-# Campo `Acceso` es insertado en la columna `AccesoPublico` de la base de datos, que es boolean. 
+# Campo `Acceso` es insertado en la columna `AccesoPublico` de la base de datos, que es boolean.
 # Sin embargo, para poder importarlo via mysqlimport es guardado en pandas como np.int64
 
 asig[Acceso].replace(to_replace = [u'Confidencial',u'Público'], value = [0, 1], inplace = True)
@@ -159,7 +159,7 @@ asig[[X,Y,X_MAGNA,Y_MAGNA]] = asig[[X,Y,X_MAGNA,Y_MAGNA]].apply(pd.to_numeric)
 for fi in [Plot, Year, X, Y, X_MAGNA, Y_MAGNA, Eq1, Eq2, Eq3, Acceso]:
     if asig[fi].dtype != np.int64:
         print "Campo {0} tiene tipo inapropiado ({1} en vez de int64).".format(fi, asig[fi].dtype)
-        
+
 for fi in [Area]:
     if asig[fi].dtype != np.float64:
         print "Campo {0} tiene tipo inapropiado ({1} en vez de float64).".format(fi, asig[fi].dtype)
@@ -168,12 +168,12 @@ for fi in [Tipo_parcela, Custodio, Custodio_abreviado, Parcela_original, Departa
     non_strings = asig[fi].dropna()[~asig[fi].dropna().apply(type).eq(unicode)]
     if len(non_strings):
         print "Campo {0} tiene tipo inapropiado ({1} en vez de unicode).".format(fi, non_strings.dtype)
-        
-    
+
+
 
 
 # ### Verificación de rango de valores
-# 
+#
 # A continuación se verifica si los valores de cada columna corresponden a los rangos esperados. En este punto del proceso sólo se verifica que los valores tengan sentido lógico. Una mayor depuración de los datos se realizará después de que éstos sean incluidos en la base de datos.
 
 # In[ ]:
@@ -192,20 +192,20 @@ if ind[D].min() < 10:
     print "Existen valores de diametro inferiores a 10 cm"
 if ind[D].max() > 770:
     print "Existen valores de diametro dudosamente altos"
-    
+
 # Rango de alturas = 1 - 100
 if ind[H].min() < 1:
     print "Existen valores de altura inferiores a 1 m"
 if ind[H].max() > 100:
     print "Existen valores de altura dudosamente altos"
 
-# Rango de densidades de madera = 0.08 - 1.39 (Global Wood Density Database compilada por Chave 
+# Rango de densidades de madera = 0.08 - 1.39 (Global Wood Density Database compilada por Chave
 # y diponible en http://datadryad.org/repo/handle/10255/dryad.235)
 if ind[Densidad].min() < 0.08:
     print "Existen valores de densidad inferiores a 0.8 gr/ml"
 if ind[Densidad].max() > 1.39:
     print "Existen valores de densidad superiores a 1.39 gr/ml"
-    
+
 # Estados de incertidumbre taxonomica = 'aff.', 'cf.', 'vel sp. aff.'
 ########################################################
 # Reemplazo de valores no aceptados detectados:
@@ -220,7 +220,7 @@ for est in ind[Estado_epiteto].dropna().unique():
         print ind[[Genero, Estado_epiteto, Epiteto]][ind[Estado_epiteto] == est]
 
 # Informacion de la columna `Habito` debe ser distribuido en tres columnas: `Habito`,
-# `Origen` y `Ecosistema`. Columnas `Origen` y `Ecosistema` deben ser creadas para tal 
+# `Origen` y `Ecosistema`. Columnas `Origen` y `Ecosistema` deben ser creadas para tal
 # proposito
 
 ind.insert(10, u'Origen', value=u'Nativa')
@@ -236,7 +236,7 @@ for hab in ind[Habito].dropna().unique():
     if hab not in [u'Arborea', u'Palma', u'Liana', u'No arborea']:
         print "{0} no es un valor aceptado de `Habito`".format(hab)
         print ind[[Genero, Epiteto, Habito]][ind[Habito] == hab]
-        
+
 for ori in ind[u'Origen'].dropna().unique():
     if ori not in [u'Nativa', u'Introducida']:
         print "{0} no es un valor aceptado de `Origen`".format(ori)
@@ -255,25 +255,25 @@ for eco in ind[u'Ecosistema'].dropna().unique():
 # Indice de parcela no debe tener duplicados
 if len(asig[asig[Plot].duplicated()]):
     print "Tabla {0} contiene indices duplicados.".format(asignacion)
-    
+
 # Verificar areas, rango permitido: 0.02-25 ha
 if asig[Area].min() < 0.02:
     print "Algunos valores de area de parcela estan por debajo del valor minimo"
 if asig[Area].max() > 25:
     print "Algunos valores de area de parcela son sospechosamente altos"
-    
+
 # Rango de años permitido: 1990-2017
 if asig[Year].min() < 1990:
     print "Realmente hay datos levantados antes de 1990?"
 if asig[Year].max() > 2017:
     print "Parece que algunos datos provienen del futuro. Verificar fechas."
 
-# Verificar correspondencia 1:1 entre el indice propio de parcela y el indice del custodio  
+# Verificar correspondencia 1:1 entre el indice propio de parcela y el indice del custodio
 if filter(lambda x: x != 1, asig.groupby([Plot, Parcela_original]).size()):
     print "No hay concordancia entre las asignaciones de indices propios y externos:"
     multiPlots = asig.groupby([Plot, Parcela_original]).size().reset_index()
     print multiPlots[multiPlots[0] > 1]
-    
+
 # Verificar tipo de parcela valido: 'temporal', 'permanente'
 for par in asig[Tipo_parcela].dropna().unique():
     if par not in [u'Permanente',u'Temporal']:
@@ -345,7 +345,7 @@ if taxdf.index[0] == 0:
 taxdf[u'Fuente'] = 1
 
 taxdf.rename(columns={Familia:u'Familia', Genero:u'Genero', Autor_genero:u'AutorGenero', Epiteto:u'Epiteto',
-    Autor_especie:u'AutorEpiteto', Habito:u'Habito'}).to_sql('Taxonomia', con, 
+    Autor_especie:u'AutorEpiteto', Habito:u'Habito'}).to_sql('Taxonomia', con,
     if_exists='append', index_label=u'TaxonID')
 
 
@@ -358,12 +358,12 @@ if u'Taxon' not in taxdf.columns:
     taxdf[u'Taxon'] = taxdf.index
 
 if 'Taxon' not in ind:
-    ind = ind.merge(taxdf[[Familia, Genero, Epiteto, u'Taxon']], on=[u'Familia',u'Genero',u'Epiteto'], 
+    ind = ind.merge(taxdf[[Familia, Genero, Epiteto, u'Taxon']], on=[u'Familia',u'Genero',u'Epiteto'],
             how='left', suffixes = ['_l', '_r']).rename(columns={Estado_epiteto:u'Incert'})
 
 if ind.index[0] == 0:
     ind.index += 1
-    
+
 ind[['Taxon', 'Incert']].to_sql('Determinaciones', con, if_exists='append', index_label='DetID')
 
 
@@ -382,12 +382,12 @@ ind[[FID, D, H, Placa, Plot , X, Y]].merge(asig[[Plot,Year]], on=Plot, how='left
 
 # Tabla Parcelas
 
-asig[[Plot, Area, Custodio, Custodio_abreviado, Proyecto, X, Y, X_MAGNA, Y_MAGNA, Departamento, 
+asig[[Plot, Area, Custodio, Custodio_abreviado, Proyecto, X, Y, X_MAGNA, Y_MAGNA, Departamento,
     Municipio, CAR, UAESPNN, Region, Escenario_referencia, Acceso]].rename(columns=
-    {Plot:'PlotID', Area:'Area', Custodio:'Custodio', Custodio_abreviado:'CustodioAbreviado', 
+    {Plot:'PlotID', Area:'Area', Custodio:'Custodio', Custodio_abreviado:'CustodioAbreviado',
     Proyecto:'Proyecto', X:'X', Y:'Y', X_MAGNA:'XMAGNA', Y_MAGNA:'YMAGNA', Departamento:'Departamento',
     Municipio:'Municipio', CAR:'CAR', UAESPNN:'UAESPNN', Region:'Region', Escenario_referencia:
-    'EscenarioReferencia', Acceso:'AccesoPublico'}).to_sql('Parcelas', con, 
+    'EscenarioReferencia', Acceso:'AccesoPublico'}).to_sql('Parcelas', con,
     if_exists='append', index=False)
 
 
@@ -402,7 +402,7 @@ if fuen.index[0] == 0:
     fuen.index += 2
 # Numeración del indice comienza en 2 porque ya existe una entrada
 # en la tabla: `Custodio`
-    
+
 fuen.rename(columns={Fuente_densidad:'Nombre'}).to_sql('Fuentes', engine, if_exists='append',
     index_label = 'FuenteID')
 
@@ -413,7 +413,7 @@ fuen.rename(columns={Fuente_densidad:'Nombre'}).to_sql('Fuentes', engine, if_exi
 # Tabla Densidades
 
 if 'Fuente' not in fuen.columns:
-    fuen['Fuente'] = fuen.index 
+    fuen['Fuente'] = fuen.index
 
 if 'Fuente' not in ind.columns:
     ind = ind.merge(fuen, how='left', on=Fuente_densidad)
@@ -431,4 +431,3 @@ dens.to_sql('Densidades', con, if_exists='append', index_label = 'DensidadID')
 
 
 con.close()
-
